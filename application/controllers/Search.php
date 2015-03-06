69ed22b9f6b38aedc0c0efa5f372dd4cb84af791
JMabbott <joelgwm@gmail.com>
2015-03-06 15:44:02 -0800

    Added getResults function to search controller
    Signed-off-by: JMabbott <joelgwm@gmail.com>

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -10,11 +10,33 @@ class Search extends Application {
 	{
 		$this->data['title'] = 'Search';
         $this->data['pagebody'] = 'search';
-        $this->render();
+		$this->render();
 	}
 	
-	public function results()
+	public function getResults()
 	{
+		$record = array();
+		$record['name'] = set_value('username');
+		$record['date'] = set_value('eventDate');
 		
+		
+		$raw_results = mysql_query("
+		SELECT * 
+		FROM events 
+		WHERE ('name' LIKE '%".$record['name']."%') 
+			OR ('description' LIKE '%".$record['name']."%')
+		UNION
+		SELECT * 
+		FROM events
+		WHERE (".$record['date']." BETWEEN 'start_date' AND 'end_date'");
+		
+		if(mysql_num_rows($raw_results) > 0)
+		{
+			//do  
+		}
+		else
+		{
+			//no results found
+		}
 	}
 }
