331e3731ddb608f8a7ef22c3b6317fb03c839edd
JulianBrandrick <julian@carmanah.net>
2015-03-06 10:56:56 -0800

    Changed config files and added controllers

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -27,32 +27,30 @@ class Register extends Application
     {
         $record = $this->users->create();
 
-        $this->form_validation->set_rules('uname', 'uname', 'required|min_length[5]|max_length[12]|is_unique[users.username]'); //|is_unique[users.username]
-        $this->form_validation->set_rules('flname', 'flname', 'required');
-        $this->form_validation->set_rules('pass1', 'pass1', 'required|matches[pass2]');
-        $this->form_validation->set_rules('pass2', 'pass2', 'required');
-        $this->form_validation->set_rules('email', 'email', 'required|valid_email|is_unique[users.email]'); //|valid_email|is_unique[users.email]
+        $this->form_validation->set_rules('username', 'User Name', 'required|min_length[5]|max_length[12]|is_unique[users.username]'); //|is_unique[users.username]
+        $this->form_validation->set_rules('fullname', 'Full Name', 'required');
+        $this->form_validation->set_rules('password1', 'Password', 'required|matches[password2]');
+        $this->form_validation->set_rules('password2', 'Password', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]'); //|valid_email|is_unique[users.email]
 
         if ($this->form_validation->run() == FALSE)
 		{
-			$this->load->view('error');
-            var_dump(validation_errors());
+			redirect('register');
 		}
 		else
 		{
-            $name = explode(' ', set_value('flname'));
+            $name = explode(' ', set_value('fullname'));
 
             $record->admin = 0;
-            $record->username = set_value('uname');
+            $record->username = set_value('username');
             $record->fname = $name[0];
             $record->lname = $name[1];
             $record->email = set_value('email');
-            $record->pword = set_value('pass1');
+            $record->pword = set_value('password1');
 
             $this->users->addUser($record);
 
-			$this->load->view('success');
-            $this->load->view('calendar');
+			redirect("login");
 		}
     }
 }
