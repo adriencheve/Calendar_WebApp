9bda6d8a00406b914a977e2823083837624c0cb6
JulianBrandrick <julian@carmanah.net>
2015-03-06 11:04:35 -0800

    Added functionality to view forms

--- application/views/Register.php
+++ application/views/Register.php
@@ -1,23 +1,27 @@
 
 <div id="container">
 
-<form method="post">  <!-- missing the action="??" part -->
+<form method="post" action="/register/validation">  <!-- missing the action="??" part -->
 
     <legend> Register Form </legend>
 
     <fieldset>
-        User Name: 
+        User Name:
         <input type="text" name="username">
+        <br>
+
+        Name:
+        <input type="text" name="fullname">
         <br>
 
         Email:
         <input type="email" name="email">
         <br>
-        
+
         Password:
         <input type="password" name="password1">
         <br>
-        
+
         Confirm Password:
         <input type="password" name="password2">
         <br>
