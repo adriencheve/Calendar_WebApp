2e8554434b0326276994f431a203e65e3beca670
JMabbott <joelgwm@gmail.com>
2015-03-08 20:31:55 -0700

    Comments
    Signed-off-by: JMabbott <joelgwm@gmail.com>

--- application/controllers/Edit.php
+++ application/controllers/Edit.php
@@ -1,5 +1,7 @@
 <?php
-
+/*
+*	Controller for the page that allows user to edit existing events.
+*/
 class Edit extends Application
 {
     function __construct()
@@ -14,9 +16,12 @@ class Edit extends Application
         $this->render();
     }
 	
+	/*
+	*	Takes form data entered by the user and updates the existing record with new data
+	*/
     function edit_event($record)
     {
-		{
+		{	
             $record->name = set_value('eventName');
             $record->start_date = set_value('startingDate');
             $record->end_date = set_value('endDate');
